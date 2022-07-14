e8d9c10dc92b1e34349ecefef24dfefc54dfe35b
Lemon Soju <yckk980320@naver.com>
2022-07-14 22:10:11 +0900

    Connect FrontEnd With BackEnd

--- src/main/reactjs/todo-react-app/src/Todo.js
+++ src/main/reactjs/todo-react-app/src/Todo.js
@@ -7,12 +7,14 @@ class Todo extends React.Component{
         super(props);
         this.state = { item: props.item, readOnly: true };
         this.delete = props.delete;
+        this.update = props.update;
     }
 
     checkboxEventHandler = (e) => {
         const thisItem = this.state.item;
         thisItem.done = !thisItem.done;
         this.setState({ item: thisItem});
+        this.update(this.state.item);
     }
 
     editEventHandler = (e) => {
@@ -24,6 +26,7 @@ class Todo extends React.Component{
     enterKeyEventHandler = (e) => {
         if (e.key === 'Enter'){
             this.setState({ readOnly: true });
+            this.update(this.state.item);
         }
     }
 
